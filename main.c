3ac06ba3d2da89f227a08c5e8e1091a02111e597
jku20 <jku-ben20@stuy.edu>
2020-10-10 14:06:24 -0400

    corrected my missreading the question

--- main.c
+++ main.c
@@ -20,13 +20,15 @@ int main() {
     }
 
     int arr2[NUM_INTS];
+    int *arr2p = arr2;
+    int *arr1p = arr1;
     for(int i = NUM_INTS-1; i >= 0; i--) {
-        *(arr2+i) = *(arr1+NUM_INTS-i-1);
+        *(arr2p+i) = *(arr1p+NUM_INTS-i-1);
     }
 
     printf("Array 2:\n");
     for(i = 0; i < NUM_INTS; i++) {
-        printf("pos %d: %d\n",i,*(arr2+i));
+        printf("pos %d: %d\n",i,*(arr2p+i));
     }
 
     return 0;
