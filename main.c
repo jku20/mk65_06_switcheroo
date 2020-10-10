ffd8297e46af3615e6bac97ab0711a10d1cfe748
jku20 <jku-ben20@stuy.edu>
2020-10-10 13:55:11 -0400

    intial commit

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_INTS 10
+
+int main() {
+    srand(time(NULL));
+
+    int arr1[NUM_INTS];
+    arr1[NUM_INTS-1] = 0;
+    int i;
+    for(i = 0; i < NUM_INTS-1; i++) {
+        arr1[i] = rand();
+    }
+
+    printf("Array 1:\n");
+    for(i = 0; i < NUM_INTS; i++) {
+        printf("pos %d: %d\n",i,arr1[i]);
+    }
+
+    int arr2[NUM_INTS];
+    for(int i = NUM_INTS-1; i >= 0; i--) {
+        *(arr2+i) = *(arr1+NUM_INTS-i-1);
+    }
+
+    printf("Array 2:\n");
+    for(i = 0; i < NUM_INTS; i++) {
+        printf("pos %d: %d\n",i,*(arr2+i));
+    }
+
+    return 0;
+}
